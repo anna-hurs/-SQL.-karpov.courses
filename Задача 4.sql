732fdc3c9212421a159b2cdf6f11b9abc70e7d51
anna-hurs <ania.khurs@yandex.ru>
2024-07-30 05:46:34 +0300

    Update Задача 4.sql

--- Задача 4.sql
+++ Задача 4.sql
@@ -12,28 +12,32 @@
 
 Поля в результирующей таблице: date, single_order_users_share, several_orders_users_share */
 
-with paying_users as (SELECT time::date as date,
-                             user_id,
-                             count(order_id) as num,
-                             case when count(order_id) = 1 then 'single'
-                                  when count(order_id) > 1 then 'several' end as type1
-                      FROM   user_actions
-                      WHERE  order_id not in (SELECT order_id
-                                              FROM   user_actions
-                                              WHERE  action = 'cancel_order')
-                      GROUP BY date, user_id), type_table as (SELECT date,
-                                               count(type1) filter (WHERE type1 = 'single') as single,
-                                               count(type1) filter (WHERE type1 = 'several') as several
-                                        FROM   paying_users
-                                        GROUP BY date, type1
-                                        ORDER BY date), total_paying as (SELECT time::date as date,
-                                        count(distinct user_id) filter (WHERE order_id not in (SELECT order_id
-                                                                                        FROM   user_actions
-                                                                                        WHERE  action = 'cancel_order')) as paying_users
-                                 FROM   user_actions
-                                 GROUP BY date), all_actions as (SELECT *
-                                FROM   type_table
-                                    LEFT JOIN total_paying using(date))
+with 
+paying_users as (SELECT time::date as date, user_id, count(order_id) as num,
+                        case when count(order_id) = 1 then 'single'
+                             when count(order_id) > 1 then 'several' end as type1
+                 FROM   user_actions
+                 WHERE  order_id not in (SELECT order_id
+                                         FROM   user_actions
+                                         WHERE  action = 'cancel_order')
+                 GROUP BY date, user_id), 
+  
+type_table as (SELECT date,
+                      count(type1) filter (WHERE type1 = 'single') as single,
+                      count(type1) filter (WHERE type1 = 'several') as several
+               FROM   paying_users
+               GROUP BY date, type1
+               ORDER BY date), 
+  
+total_paying as (SELECT time::date as date,
+                        count(distinct user_id) filter (WHERE order_id not in (SELECT order_id
+                                                                               FROM   user_actions
+                                                                               WHERE  action = 'cancel_order')) as paying_users
+                 FROM   user_actions
+                 GROUP BY date), 
+  
+all_actions as (SELECT * FROM type_table LEFT JOIN total_paying using(date))
+
 SELECT date,
        round((max(single::decimal/paying_users*100)), 2) as single_order_users_share,
        round((max(several::decimal/paying_users*100)), 2) as several_orders_users_share
